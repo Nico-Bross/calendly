6ab3d9e882429a1f5d668e096fe2eda0774bb60d
studbrosle3454 <leonnicolas.bross@stud.th-rosenheim.de>
2022-07-19 16:07:17 +0200

    Initial Commit

---
+++ Shared/calandarApp.swift
@@ -0,0 +1,17 @@
+//
+//  calandarApp.swift
+//  Shared
+//
+//  Created by Nico Bro on 19.07.22.
+//
+
+import SwiftUI
+
+@main
+struct calandarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
