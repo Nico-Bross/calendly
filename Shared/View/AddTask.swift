4a78a152031cb64fa902587b3378638bb7d0595e
nbross <miry_cookie0e@icloud.com>
2022-07-27 18:44:54 +0200

    added Taskts

---
+++ Shared/View/AddTask.swift
@@ -0,0 +1,20 @@
+//
+//  AddTask.swift
+//  calandar (iOS)
+//
+//  Created by Nico Bro on 22.07.22.
+//
+
+import SwiftUI
+
+struct AddTask: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTask_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
